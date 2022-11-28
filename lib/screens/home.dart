6dd4a2a1eb0c0ac8eef2cb77dc2d9b464b835fdb
Junior Cabrera <junior.cabrera@itksquare.edu.mx>
2022-11-28 16:23:07 -0400

    Feature: 2nd

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,29 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_class/controllers/theme_mode_provider.dart';
+import 'package:provider_class/screens/second_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-  final modeThemeProvider = Provider.of<ThemeModeProvider>(context);
+    final modeThemeProvider = Provider.of<ThemeModeProvider>(context);
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: modeThemeProvider.primaryColor,
       ),
-      body: Container(
-        color: modeThemeProvider.backgroundColor,
+      body: Center(
+        child: Container(
+          color: modeThemeProvider.backgroundColor,
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SecondScreen()),
+              );
+            },
+            child: const Text('Next Screen'),
+          ),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-          backgroundColor:modeThemeProvider.primaryColor,
-          child: modeThemeProvider.iconFloatingElevationButton,
-          onPressed: () {
-            modeThemeProvider.primaryColor == Colors.orange
-            ? modeThemeProvider.changeThemeToDark()
-            : modeThemeProvider.changeThemeToLight();
-          }),
     );
   }
 }
